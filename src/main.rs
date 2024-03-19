a45693053b4e42af7d67cd7630f8d136aa089349
Jedjoud10 <jribijed6@gmail.com>
2024-03-18 23:17:40 -0400

    The rotting begins

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+// 3 registers
+// Simple IO (chat)
+// Graphics capable
+// Multithreadable or Parallelizable
+// Weird operations
+// Conditionals, loops, scopes
+// Vector additions and vector math
+// Output 480x480 bitmask
+
+// REGISTERS
+// 2 registers that can swap types. either float or integer
+// let __a__ be __b__ __c__
+// __a__: me/you
+// __b__: inting/fr
+// __c__: rn,bruh,
+// let you be inting bruh
+// let me be fr rn
+// fr = float
+// inting = integer
+
+// TYPE CONDITIONALS
+// is __a__ __b__?:
+//     nuhuh, gtfo 
+//     ofcbruh, gtfo
+// __a__: me/you
+// __b__: inting/fr
+// is you being fr?
+// is me inting?
+
+// GOTOS:
+// gtfo incel goes to last incelpilled marker only if "you" register is real, if not, calls normie call
+// gtfo normie goes to last normiepilled marker only if "me" register is inting if not, calls incel call,
+// gtfo bruh goes to last bruh marker only if all registers are inting, if not, crashes the program
+
+// MATH:
+// lemme rizz you __a__
+// __a__: up/down
+// Adds or subtracts the number from 0-10 to the you register. uses current value of "me" register as seed
+// lemme square you __a__
+// __a__: up/down
+// Squares or square-roots the you register
+
+
+// IO
+// output:
+// __a__ in the chat
+// __a__: ws/ls
+// Prints result of last successful operation or of last failed operation (ws=success, ls=fail)
+
+// input: 
+// chat, is this real?
+// saves input value to all real regs
+
+// chat, is this intmaxxing?
+// saves input value to all int regs
+
+
+fn main() {
+}
