740fd5826103341db436355e60014780ffa64a46
Jedjoud10 <jribijed6@gmail.com>
2024-03-19 21:15:04 -0400

    It's getting worse

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,112 @@
-// 3 registers
-// Simple IO (chat)
-// Graphics capable
-// Multithreadable or Parallelizable
-// Weird operations
-// Conditionals, loops, scopes
-// Vector additions and vector math
-// Output 480x480 bitmask
-
-// REGISTERS
-// 2 registers that can swap types. either float or integer
-// let __a__ be __b__ __c__
-// __a__: me/you
-// __b__: inting/fr
-// __c__: rn,bruh,
-// let you be inting bruh
-// let me be fr rn
-// fr = float
-// inting = integer
-
-// TYPE CONDITIONALS
-// is __a__ __b__?:
-//     nuhuh, gtfo 
-//     ofcbruh, gtfo
-// __a__: me/you
-// __b__: inting/fr
-// is you being fr?
-// is me inting?
-
-// GOTOS:
-// gtfo incel goes to last incelpilled marker only if "you" register is real, if not, calls normie call
-// gtfo normie goes to last normiepilled marker only if "me" register is inting if not, calls incel call,
-// gtfo bruh goes to last bruh marker only if all registers are inting, if not, crashes the program
-
-// MATH:
-// lemme rizz you __a__
-// __a__: up/down
-// Adds or subtracts the number from 0-10 to the you register. uses current value of "me" register as seed
-// lemme square you __a__
-// __a__: up/down
-// Squares or square-roots the you register
-
-
-// IO
-// output:
-// __a__ in the chat
-// __a__: ws/ls
-// Prints result of last successful operation or of last failed operation (ws=success, ls=fail)
-
-// input: 
-// chat, is this real?
-// saves input value to all real regs
-
-// chat, is this intmaxxing?
-// saves input value to all int regs
-
+enum Register {
+    Real(f32),
+    Int(i32),
+}
 
 fn main() {
+    let file = include_str!("./test.rot");
+    let lines = file.lines().collect::<Vec<&str>>();
+    let mut me = Register::Real(0.0f32);
+    let mut you = Register::Real(0.0f32);
+    let mut last_success = String::new();
+    let mut last_failure = String::new();
+
+    macro_rules! quit {
+        ($a:expr) => {
+            last_failure = uwuifier::uwuify_str_sse($a)
+        };
+    }
+
+    macro_rules! out {
+        ($a:expr) => {
+            last_success = uwuifier::uwuify_str_sse($a)
+        };
+    }
+
+    for line in lines {
+        let words = line.split_whitespace().collect::<Vec<&str>>();
+
+        if words.len() == 0 {
+            continue;
+        }
+
+        // REGISTER DEFINITION
+        if words[0] == "let" {
+            let reg = match words[1] {
+                "you" => &mut you,
+                "me" => &mut me,
+                _ => { quit!("Failed to fetch register type. I don't know why!"); continue },
+            };
+
+            let bytes = match reg {
+                Register::Real(x) => x.to_ne_bytes(),
+                Register::Int(x) => x.to_ne_bytes(),
+            };
+
+            match words[3] {
+                "fr" => *reg = Register::Real(f32::from_ne_bytes(bytes)),
+                "inting" => *reg = Register::Int(i32::from_ne_bytes(bytes)),
+                _ => quit!("Failed to fetch register target!"),
+            };
+
+            out!("Changed register type! I don't know which one but surely at least one!")
+        }
+
+        // IO OUTPUT
+        if line.ends_with("in the chat") {
+            match words[0] {
+                "ws" => println!("{}", last_success),
+                "ls" => println!("{}", last_failure),
+                _ => {}
+            }
+        }
+
+        // IO INPUT
+        if line.starts_with("chat, is this") {
+            println!("{}", line.trim_start_matches("chat, "));
+            let input = std::io::stdin().lines().next().unwrap().unwrap();
+            let float = input.parse::<f32>().unwrap_or_default();
+            let int = input.parse::<i32>().unwrap_or_default();
+
+            match words[3] {
+                "real?" => {
+                    if let Register::Real(x) = &mut you {
+                        *x = float;
+                    }
+
+                    if let Register::Real(x) = &mut me {
+                        *x = float;
+                    }
+                },
+                "inting?" => {
+                    if let Register::Int(x) = &mut you {
+                        *x = int;
+                    }
+
+                    if let Register::Int(x) = &mut me {
+                        *x = int;
+                    }
+                },
+                _ => { quit!("Failed to fetch output register target"); continue }
+            }
+        }
+
+        // MATH
+        if line.starts_with("lemme rizz you") {
+            let offset = match words[3] {
+                "up" => 1,
+                "down" => -1,
+                "bruh" => 0,
+                _ => { quit!("Not a fucking rizz option bruh!!!"); 0 },
+            };
+
+            let out = match &mut you {
+                Register::Real(x) => { *x += offset as f32; format!("{x}") },
+                Register::Int(x) => { *x += offset; format!("{x}") },
+            };
+
+            let input = format!("Changed value of \"you\" register to {}", out);
+            out!(&input);
+        }
+    }
 }
